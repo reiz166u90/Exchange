6a968aaf8c645f8f3e67f9cf8902550ab404fb50
unknown <reired63@gmail.com>
2022-11-29 21:07:32 +0800

    Ree

--- myscripts.js
+++ myscripts.js
@@ -3,55 +3,68 @@
 //captchaArray = ['4+9', '4*8', '7-3', '5*9', '8/2', '11-5'];
 //exchangeArray = [2870, 3120, 3500, 2.3, 123, 11];
 
+        
 var btn = document.querySelectorAll(".r > div");
-var inp = document.querySelector("input");
-btn.forEach(val=>{
-    val.addEventListener("click", ()=>{
-        inp.value+=val.innerText;
-    })
-})
+var inp = document.getElementById ('myInput');
+var in1 = document.getElementById ('inputCaptcha');
+var in2 = document.getElementById ('total_price');
 
-
-function myFunction(){
-    var pic;
-    var form = parseInt(document.getElementById('myInput').value);
-    var city = document.getElementById('number_select').value;
-    if(city == 'USD'){
-        parseFloat(document.getElementById('total_price').value = form * 2870+" MNT");
-    }
-    else if(city == 'EURO'){
-        parseInt(document.getElementById('total_price').value = form * 3120+" MNT");
-    }
-    else if(city == 'POUND'){
-        parseInt(document.getElementById('total_price').value = form * 3500+" MNT");
-    }
-    else if(city == 'WON'){
-        parseInt(document.getElementById('total_price').value = form * 2.3+" MNT");
-    }
-    else if(city == 'YEN'){
-        parseInt(document.getElementById('total_price').value = form * 123+" MNT");
-    } 
-    else if(city == 'YAN'){
-        parseInt(document.getElementById('total_price').value = form * 11+" MNT");
-    }
+function toonemeh(a){
+    inp.value += a;
 }
-    var allValue = ["4+9", "4*8", "7-3", "5*9", "8/2", "11-5"];
+
+function dorjoogoy(a){
+   if(a == '1dehtoo'){
+    inp = document.getElementById('inputCaptcha');
+   } else {
+    inp = document.getElementById ('myInput');
+   }
+}
+
+var allValue = ["4 + 9", "4 * 8", "7 - 3", "5 * 9", "8 / 2", "11 - 5"];
     var cVal1 = allValue[Math.floor(Math.random() * allValue.length)];
+
+        function myFunction(){
+            if(eval(String(cVal1)) == parseInt(document.getElementById('inputCaptcha').value)){
+            console.log('taarsan');
+            var pic;
+            var form = parseInt(document.getElementById('myInput').value);
+            var city = document.getElementById('number_select').value;
+            if(city == 'USD'){
+                parseFloat(document.getElementById('total_price').value = form * 2870+" MNT");
+            }
+            else if(city == 'EURO'){
+                parseInt(document.getElementById('total_price').value = form * 3120+" MNT");
+            }
+            else if(city == 'POUND'){
+                parseInt(document.getElementById('total_price').value = form * 3500+" MNT");
+            }
+            else if(city == 'WON'){
+                parseInt(document.getElementById('total_price').value = form * 2.3+" MNT");
+            }
+            else if(city == 'YEN'){
+                parseInt(document.getElementById('total_price').value = form * 123+" MNT");
+            } 
+            else if(city == 'YAN'){
+                parseInt(document.getElementById('total_price').value = form * 11+" MNT");
+            }
+        }
+    else{
+        console.log('taarsangui')
+        alert('captcha taarsanq')
+        window.location = window.location.href;
+    }
+    
+
+    }
     //alert(cVal1)
     
     captchaValue.innerHTML = cVal1
-    /*thisValue = "";
+    
+    thisValue = "";
     inputCaptcha.addEventListener('change', function(){
         thisValue = inputCaptcha.value;
         //alert(thisValue);
     })
 
-    submitBtn.addEventListener('click', function(){
-        if(cVal1 == parseInt(document.getElementById('inputCaptcha').value)){
-            alert('Valid');
-            document.logForm.submitBtn();
-        }
-        else if(cVal1 != parseInt(document.getElementById('inputCaptcha').value)){
-            alert('Invalid');
-        }
-    })*/
+    
